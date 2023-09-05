2685a3579170cfea708e9bba1e81a11923155785
Görkem Karagöl <77544230+gorkemkaragl@users.noreply.github.com>
2023-09-05 20:37:13 +0300

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,3 +1,21 @@
+"""
+We need to prepare the data to feed the network: we have - data/masks, data/images - directories where we prepared masks and input images. Then, convert each file/image into a tensor for our purpose.
+
+We need to write two functions in src/preprocess.py:
+    - one for feature/input          -> tensorize_image()
+    - the other for mask/label    -> tensorize_mask()
+
+
+Our model will accepts the input/feature tensor whose dimension is
+[batch_size, output_shape[0], output_shape[1], 3]
+&
+the label tensor whose dimension is
+[batch_size, output_shape[0], output_shape[1], 2].
+
+At the end of the task, our data will be ready to train the model designed.
+
+"""
+
 import glob
 import cv2
 import torch
@@ -6,36 +24,57 @@ from constant import *
 
 
 def tensorize_image(image_path_list, output_shape, cuda=False):
-    # Create empty list
+    """
+    Parameters
+    ----------
+    image_path_list : list of strings
+        [“data/images/img1.png”, .., “data/images/imgn.png”] corresponds to
+        n images to be trained each step.
+    output_shape : tuple of integers
+        (n1, n2): n1, n2 is width and height of the DNN model’s input.
+    cuda : boolean, optional
+        For multiprocessing,switch to True. The default is False.
+
+    Returns
+    -------
+    torch_image : Torch tensor
+        Batch tensor whose size is [batch_size, output_shape[0], output_shape[1], C].       For this case C = 3.
+    """
+    #Boş bir liste oluşturuldu
     local_image_list = []
 
-    # For each image
+    #Her resim için
     for image_path in image_path_list:
 
-        print(image_path)
-        # Access and read image
+        #Görüntü okundu
         image = cv2.imread(image_path)
 
-        # Resize the image according to defined shape
+        #Görüntü yeniden boyutlandırıldı
         image = cv2.resize(image, output_shape)
-
-        # Change input structure according to pytorch input structure
+        
+        """
+        print("image_path \n",image)
+        print("deneme \n",image[:,:,0])
+        print("image[:,:,0].shape",image[:,:,0].shape)
+        print(image.shape)
+        print(image.dtype)
+        """
+            
         torchlike_image = torchlike_data(image)
-
-        # Add into the list
+        
         local_image_list.append(torchlike_image)
 
-    # Convert from list structure to torch tensor
-
+    
     image_array = np.array(local_image_list, dtype=np.float32)
     torch_image = torch.from_numpy(image_array).float()
+    
 
     # If multiprocessing is chosen
     if cuda:
         torch_image = torch_image.cuda()
-
+        
     return torch_image
-
+    
 def tensorize_mask(mask_path_list, output_shape, n_class, cuda=False):
     # Create empty list
     local_mask_list = []
@@ -57,85 +96,143 @@ def tensorize_mask(mask_path_list, output_shape, n_class, cuda=False):
 
         local_mask_list.append(torchlike_mask)
 
-    mask_array = np.array(local_mask_list, dtype=np.int64)
+    mask_array = np.array(local_mask_list, dtype=int)
     torch_mask = torch.from_numpy(mask_array).float()
     if cuda:
         torch_mask = torch_mask.cuda()
 
     return torch_mask
 
-
 def image_mask_check(image_path_list, mask_path_list):
 
-
-    # Check list lengths
+    
     if len(image_path_list) != len(mask_path_list):
-        print("There are missing failes ! Images and masks folder should have some number of files.")
+        print("There are missing files ! Images and masks folder should have same number of files.")
         return False
+
+    
+    for image_path, mask_path in zip(image_path_list, mask_path_list):
+        image_name = image_path.split('\\')[-1].split('.')[0]
+        mask_name  = mask_path.split('\\')[-1].split('.')[0]
+        if image_name != mask_name:
+            print("Image and mask name does not match {} - {}".format(image_name, mask_name)+"\nImages and masks folder should have same file names." )
+            return False
 
     return True
 
 ############################ TODO ################################
 def torchlike_data(data):
+    """
+    Change data structure according to Torch Tensor structure where the first
+    dimension corresponds to the data depth.
+
+
+    Parameters
+    ----------
+    data : Array of uint8
+        Shape : HxWxC.
+
+    Returns
+    -------
+    torchlike_data_output : Array of float64
+        Shape : CxHxW.
+
+    """
+
+    n_channels = data.shape[2]    ### 3
+
+    torchlike_data_output = np.empty((n_channels,data.shape[0],data.shape[1]))
+  
+    
     
 
-    # Obtain channel value of the input
-    n_channels = data.shape[2]
+    for i in range(n_channels):
+        
 
-    # Create and empty image whose dimension is similar to input
-    torchlike_data_output = np.empty((n_channels, data.shape[0], data.shape[1]))
-    for ch in range(n_channels):
-        torchlike_data_output[ch] = data[:,:,ch]
-
+        torchlike_data_output[i] = data[:,:,i]
     return torchlike_data_output
 
 def one_hot_encoder(data, n_class):
-    global encoded_label
-    
-   
-    encoded_label = np.zeros((data.shape[0], data.shape[1], n_class), dtype=np.int32)  # Updated dtype
-   
-    global unique_values
-    unique_values = np.unique(data)
+    """
+    Returns a matrix containing as many channels as the number of unique
+    values ​​in the input Matrix, where each channel represents a unique class.
 
-    for i, unique_value in enumerate(np.unique(data)):
-        encoded_label[:, :, i][data == unique_value] = 1
-    return encoded_label
-    
+
+    Parameters
+    ----------
+    data : Array of uint8
+        2D matrix.
+    n_class : integer
+        Number of class.
+
+    Returns
+    -------
+    encoded_data : Array of int64
+        Each channel labels for a class.
+
+    """
+    if len(data.shape) != 2:
+        print("It should be same with the layer dimension, in this case it is 2")
+        return
+    if len(np.unique(data)) != n_class:
+        print("The number of unique values ​​in 'data' must be equal to the n_class")
+        return
+
+    # Boyutu (genişlik, yükseklik, sınıf_sayısı) olan diziyi tanımlandı
+    encoded_data = np.zeros((*data.shape, n_class), dtype=int)
+
+    # Etiker(label) tanımlandı
+    encoded_labels = [[0,1],[1,0]]
+
+    #
+    for lbl in range(n_class):
+        encoded_label = encoded_labels[lbl]
+       
+        numerical_class_inds = data[:,:] == lbl 
+       
+        
+        encoded_data[numerical_class_inds] = encoded_label 
+        
+        
+    return encoded_data
 ############################ TODO END ################################
-
-
 
 
 
 if __name__ == '__main__':
 
-    # Access images
+    # Görüntüler okundu ve sıralandı
     image_list = glob.glob(os.path.join(IMAGE_DIR, '*'))
     image_list.sort()
 
-
-    # Access masks
+    # Maskeler okundu ve sıralandı
     mask_list = glob.glob(os.path.join(MASK_DIR, '*'))
     mask_list.sort()
 
 
-    # Check image-mask match
+    # Görüntü ve maskelerin isimlerinin kontrolü yapıldı
     if image_mask_check(image_list, mask_list):
-       
-       
-       batch_image_list = image_list[:BATCH_SIZE]
-       batch_image_tensor = tensorize_image(batch_image_list, (224, 224))
 
-       print("For features:\ndtype is "+str(batch_image_tensor.dtype))
-       print("Type is: "+str(type(batch_image_tensor)))
-       print("The size should be ["+str(BATCH_SIZE)+", 3, "+str(HEIGHT)+" , "+str(WIDTH)+"]")
-       print("Size is "+str(batch_image_tensor.shape)+"\n")
+        # Belirtilen batch_size değişkeni kadar görüntü bir listeye kaydedildi
+        batch_image_list = image_list[:BACTH_SIZE]
 
-       batch_mask_list = mask_list[:BATCH_SIZE]
-       batch_mask_tensor = tensorize_mask(batch_mask_list, (HEIGHT, WIDTH),2)
-    
-       print("For labels:\ndtype is"+str(batch_mask_tensor.dtype))
-       print("Type is: "+str(type(batch_mask_tensor)))
-       print("The size should be ["+str(BATCH_SIZE)+", 2, "+str(HEIGHT)+" , "+str(WIDTH)+"]")
-       print("Size is "+str(batch_mask_tensor.shape))  
+        # Torch tensorune dönüştürüldü
+        batch_image_tensor = tensorize_image(batch_image_list, (224, 224))
+
+        # Kontroller
+        print("For features:\ndtype is "+str(batch_image_tensor.dtype))
+        print("Type is "+str(type(batch_image_tensor)))
+        print("The size should be ["+str(BACTH_SIZE)+", 3, "+str(HEIGHT)+", "+str(WIDTH)+"]")
+        print("Size is "+str(batch_image_tensor.shape)+"\n")
+
+        # Belirtilen batch_size değişkeni kadar maske bir listeye kaydedildi
+        batch_mask_list = mask_list[:BACTH_SIZE]
+
+        # Torch tensorune dönüştürüldü
+        batch_mask_tensor = tensorize_mask(batch_mask_list, (HEIGHT, WIDTH), 2)
+
+        # Kontrol
+        print("For labels:\ndtype is "+str(batch_mask_tensor.dtype))
+        print("Type is "+str(type(batch_mask_tensor)))
+        print("The size should be ["+str(BACTH_SIZE)+", 2, "+str(HEIGHT)+", "+str(WIDTH)+"]")
+        print("Size is "+str(batch_mask_tensor.shape))
